c037035d68adbe5514432c06636211a99145973b
Andrew Miller <andrew.miller@ocado.com>
2012-02-15 10:05:54 +0000

    index/contact jsons working - need to do content for each page

--- personal.py
+++ personal.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for
-import urllib2,json
+import urllib2,json,copy
 
 app = Flask(__name__)
 
@@ -7,6 +7,8 @@ app = Flask(__name__)
 def index():
 	links = {'home':('home',url_for('index')),'contact':('contact',url_for('contact')),'blog':('blog',url_for('blog'))}
 	content = json.load(open('./static/index.json','r'))
+	main_content = copy.deepcopy(content)
+#	del['page']
 	return render_template('index.html',links=links, content=content)
 
 @app.route('/contact')
