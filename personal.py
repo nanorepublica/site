166a9ed7dce91b264a33029c7192aaef5e0bab83
Andrew <nanorepublica@gmail.com>
2012-02-17 22:27:34 +0000

    form rendered - semi works

--- personal.py
+++ personal.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect,render_template_string,get_template_attribute
 from flaskext.wtf import Form, TextField, validators, TextAreaField
 from flaskext.wtf.html5 import EmailField
 import urllib2,json,copy,re
-from jinja2 import evalcontextfilter,Environment
+#from jinja2 import evalcontextfilter,Environment
+from flaskext.mail import Mail, Message
+
 
 app = Flask(__name__)
 app.secret_key = 'm\xb2q\x19=\xc0S\xe9w\x19\xcd\x14\xb7\xa7x\xa4U\xdb<\xfb\x87\xc7C)'
+mail = Mail(app)
 
 class contactForm(Form):
 	name = TextField('Name:',[validators.Required()])
@@ -13,19 +16,15 @@ class contactForm(Form):
 	message = TextAreaField('Message:',[validators.Required()])
 
 @app.template_filter('split')
-def split(value):
+def split(value,contact):
 	pattern = '(\{\{[a-zA-Z\.\_\(\)\ ]*\}\})'
-	result = re.split(pattern,value)	
-#	i = re.sub(pattern,'\g<1>',value)
-	print result
+	result = re.split(pattern,value)
+	render_field = get_template_attribute('_helper.html','render_field')
+	t = ''
 	for i in result:
-		if not len(i):
-#			print i
-			result.remove(i)
-	print result
-	return render_template_string(''.join(result))
-
-
+		if len(i):
+			t = '%s%s' % (t,render_template_string(i,contact=contact,render_field=render_field))
+	return t
 
 @app.route('/')
 def index():
@@ -35,15 +34,15 @@ def index():
 
 @app.route('/contact', methods=("GET", "POST"))
 def contact():
-	contact = contactForm()
+	contactf = contactForm()
 	links = {'home':('home',url_for('index')),'contact':('contact',url_for('contact')),'blog':('blog',url_for('blog'))}
-	#content = open('./static/contact.content','r').read()
 	content = json.loads(''.join(''.join(open('./static/contact.json','r').read().splitlines()).split('\t')))
-	if contact.validate_on_submit():
-		flash("Success")
+	if contactf.validate_on_submit():
+		msg = Message("New mail from %s" % contactf.data.name,sender=contactf.data.email,recipients=["info@akmiller.co.uk"],body=contactf.data.message)
+		mail.send(msg)
 		return redirect(url_for("index"))
 		#pass #send an email to me & alter content to display success of some kind
-	return render_template('index.html',links=links, content=content,contact=contact)
+	return render_template('index.html',links=links, content=content,contact=contactf)
 
 @app.route('/test')
 def test():
